2bb89d5deed45c2274fecf4f7eeb188b37c13b4b
Sebastian Ohde <srohde1@buffs.wtamu.edu>
2020-11-16 18:19:53 -0500

    Ready for grading

--- birthstone.py
+++ birthstone.py
@@ -1,2 +1,29 @@
 # Follow the assignment instructions to code an app that
 # will tell a user their birthstone.
+user_input = input("Enter the number of the month you were born (1-12): ")
+month = int(user_input)
+
+if month == 1:
+    print("Your birthstone is garnet")
+if month == 2:
+    print("Your birthstone is amethyst")
+if month == 3:
+    print("Your birthstone is bloodstone")
+if month == 4:
+    print("Your birthstone is diamond")
+if month == 5:
+    print("Your birthstone is emerald")
+if month == 6:
+    print("Your birthstone is pearl")
+if month == 7:
+    print("Your birthstone is ruby")
+if month == 8:
+    print("Your birthstone is sardonyx")
+if month == 9:
+    print("Your birthstone is sapphire")
+if month == 10:
+    print("Your birthstone is opal")
+if month == 11:
+    print("Your birthstone is topaz")
+if month == 12:
+    print("Your birthstone is turquoise or lapis")
